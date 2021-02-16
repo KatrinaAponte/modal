5982ed92f97a49f7b2d524b9547544bb851779e3
KatrinaAponte <contact.katrinaaponte.com>
2021-02-16 15:28:19 -0800

    Removed logo from header section.

--- header.php
+++ header.php
@@ -57,7 +57,6 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
