3de6854daf2194b42895acca858d1820aad032df
KatrinaAponte <contact.katrinaaponte.com>
2021-02-24 15:39:05 -0800

    Added navigation menu to footer.php file.

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'modal' ) ); ?>">
+			<a href="<?php echo esc_url( __( 'https://katrinaaponte.com//themes/modal', 'modal' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'modal' ), 'WordPress' );
@@ -25,6 +25,17 @@
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'modal' ), 'modal', '<a href="http://www.katrinaaponte.com">Katrina Aponte</a>' );
 				?>
 		</div><!-- .site-info -->
+
+		<?php if ( has_nav_menu( 'footer' ) ) : ?>
+			<nav id="footer-navigation" class="footer-navigation">
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'footer',
+						'menu_id'		 => 'footer',
+					)
+				);
+				?>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
